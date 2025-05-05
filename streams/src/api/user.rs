ddf47a89c30a96c648ba87ac3c19d0050c624595
Dyrell C <dyrell@demia.net>
2025-05-05 17:26:46 -0600

    Add filter_messages and sync_base_branch

--- streams/src/api/user.rs
+++ streams/src/api/user.rs
@@ -608,6 +608,13 @@ where
     pub fn messages(&mut self) -> Messages<T> {
         Messages::new(self)
     }
+    
+    /// Start a [`Messages`] stream to traverse the channel messages with a filter
+    /// 
+    /// See the documentation in [`Messages`] for more details and examples.
+    pub fn filtered_messages<'a>(&'a mut self, filter: &'a str) -> Messages<'a, T> {
+        Messages::new_with_filter(self, filter)
+    }
 
     /// Iteratively fetches all the next messages until internal state has caught up
     ///
@@ -615,6 +622,17 @@ where
     pub async fn sync(&mut self) -> Result<usize> {
         // ignoring the result is sound as Drain::Error is Infallible
         self.messages()
+            .try_fold(0, |n, _| future::ok(n + 1))
+            .await
+            .map_err(Error::Messages)
+    }
+    
+    /// Synchronize the base branch of the Stream 
+    /// 
+    /// If succeeded, returns the number of messages advanced.
+    pub async fn sync_base_branch(&mut self) -> Result<usize> {
+        let base_branch = self.base_branch().to_string();
+        self.filtered_messages(base_branch.as_str())
             .try_fold(0, |n, _| future::ok(n + 1))
             .await
             .map_err(Error::Messages)
