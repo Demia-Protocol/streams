3f62fa14216d5c68015344e596fe4816678a896e
Dyrell C <dyrell@demia.net>
2025-05-15 23:50:18 -0600

    use read instead of write for encrypt/decrypt

--- lets/src/id/identifier.rs
+++ lets/src/id/identifier.rs
@@ -431,7 +431,7 @@ where
 
                         // Create an AEAD Encryption packet to be received and processed by the recipient
                         let encrypted_data = stronghold
-                            .write()
+                            .read()
                             .await
                             .x25519_encrypt(xkey, sender_location, key.to_vec())
                             .await
